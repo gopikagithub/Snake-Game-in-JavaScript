e31142714dd20910e909e0ef3f6730e0653b8b07
gopikagithub <87468529+gopikagithub@users.noreply.github.com>
2022-11-02 12:16:17 +0400

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ let yVelocity=0;
 let foodX;
 let foodY;
 let score=0;
-let timeInterval=300;
+let timeInterval=250;
 let lastSnake;
 
 let snake=[{x:unitSize*4,y:0},{x:unitSize*3,y:0},{x:unitSize*2,y:0},{x:unitSize,y:0},{x:0,y:0}];
@@ -108,8 +108,10 @@ function moveSnake(){
     if(snake[0].x==foodX && snake[0].y==foodY){
             score+=10;
             scoreText.textContent=`Score : ${score}`;
-            if(timeInterval>=90)
-            timeInterval-=20;
+            if(timeInterval>=100)
+            timeInterval-=40;
+            if(score==150)
+                timeInterval-=40;
             createFood();
         }
     else{
